7f161f710859e5709a76505fbba2e7e98b91e244
qiuhaiqiuhai <qiuhaiqiuhai@126.com>
2015-10-23 09:23:37 +0800

    profile edit

--- profile.php
+++ profile.php
@@ -7,6 +7,7 @@ include "members_only.php"
 <html>
 <head>
     <link rel="stylesheet" href="css/profile.css">
+    <script type="text/javascript" src="JS/pwconfirm.js"></script>
 </head>
 <body>
 
@@ -22,12 +23,77 @@ include "members_only.php"
 
 <section>
 <?php 
+    if(isset($_GET['edit'])){
+      echo 'You are logged in as: '.$_SESSION['valid_user'].' <br />';
+      if(isset($_GET['username_exist']))
+        echo 'username exist';
+  // echo "<br>" .$query. "<br>";
+      $query = 'select * from users_account '
+             ."where userID=".$_SESSION['valid_userID'];
+  // echo "<br>" .$query. "<br>";
+      $result = $db->query($query);
+      $row = $result->fetch_assoc();
+
+      echo 'Name:
+            <input type=text name="name" id="name" required="required" form="submit_edit" value = "'.$row['name'].'"><br />';
+      echo 'E-mail:
+            <input type=email name="email" id="email" required="required" form="submit_edit" value = "'.$row['email'].'"><br />';
+      echo 'E-mail:
+            <input type=email name="email" id="email" required="required" form="submit_edit" value = "'.$row['email'].'"><br />';
+      echo 'Password:
+            <input type=password name=password id="password" form="submit_edit" required="required" ><br />';
+      echo 'Password confirmation:
+            <input type=password name=password2 id="password2" required="required" onkeyup="checkPass();" >
+            <span id="confirmMessage" class="confirmMessage"></span><br />';
+      echo 'Gender:
+            <select name="gender" required="required" form="submit_edit" value= "'.$row['gender'].'">
+                <option value="Male">Male</option>
+                <option value="Female" '.(("Female"==$row['gender'])?' selected':'').'>Female</option>
+            </select>
+            <br>';
+      echo 'Birthday:
+            <input type="date" name="birthdate" required="required" form="submit_edit" value="'.$row['birthdate'].'"
+            ><br />';
+
+      $Cities = array(
+         "Tokyo",
+         "Mexico City",
+         "New York City",
+         "Mumbai",
+         "Seoul",
+         "Shanghai",
+         "Lagos",
+         "Sao Paulo",
+         "Cairo",
+         "London",
+         "Singapore");
+      echo 'City:
+            <select name="city" id="listBox" required="required" form="submit_edit" value="'.$row['city'].'">';
+      foreach($Cities as $city){
+            echo '<option value='.$city.(($city==$row['city'])?' selected':'').'>'.$city.'</option>';
+            }
+      echo '</select><br />';
+
+      echo 'Height(cm)<input type=number name="height" value='.$row['height'].' min=0 max=250 form="submit_edit" required="required"><br />';
+      echo 'Education:<input type=text name="education" form="submit_edit" required="required" value = "'.$row['education'].'"><br />';
+
+      if ($row['profilePhoto']!=Null )
+      {
+          echo '<img src=users_profile_photo/'.$row['profilePhoto'].' height="100">';
+      }elseif($row['gender'] == 'Male'){
+          echo '<img src=users_profile_photo/default_male.jpg>';
+      }else{
+          echo '<img src=users_profile_photo/default_female.jpg>';
+      }
+
+    }else{
     print_basic_info( $_SESSION['valid_user'], $db);
+    }
 
 ?>
 <form action="profile_action.php" method=POST enctype="multipart/form-data">
     <input type="file" name=profilePhoto accept="image/*">
-    <input type=submit name=submit value="Change Profile photo" >
+    <input type=submit name=profilePhoto value="Change Profile photo" >
 </form>
 
 description: <br/>
@@ -38,11 +104,18 @@ $query = 'select * from users_description '
 // echo "<br>" .$query. "<br>";
 
     $result = $db->query($query);
+    $description = "N/A";
+
     if($result->num_rows >0 ){
         $row = $result->fetch_assoc();
-        echo $row['description'];
+        $description = $row['description'];
+    }
+
+    if(isset($_GET['edit'])){
+      echo '<textarea name="description" form="submit_edit" rows="4" cols="50">'.$row['description'].'
+            </textarea>';
     }else{
-        echo "N/A";
+      echo $description;
     }
 
 ?>
@@ -67,14 +140,23 @@ photos:
 
 <form action="profile_action.php" method=POST enctype="multipart/form-data">
     <input type="file" name="photo" accept="image/*" >
-    <input type=submit name=submit value=Upload >
+    <input type=submit name=photo value=Upload >
 </form>
     
 </section>
     
-<form action="register.php" method=POST >
-    <input type=submit name=submit value="Edit Profile" >
-</form>
+<?php if(isset($_GET['edit'])){
+        echo '<form action="profile_action.php" method=POST id="submit_edit" onclick="return checkOnSubmit();">
+              <input type=submit name="submit_edit" value="submit" >
+              </form>';
+      }else{
+        echo '<form action="profile.php" method=GET >
+              <input type=submit name="edit" value="Edit Profile">
+              </form>';
+      }
+
+
+?>
     
     
 <aside>
--- profile_action.php
+++ profile_action.php
@@ -4,7 +4,7 @@ include "members_only.php";
 $name=$_SESSION['valid_user']; 
 $userID=$_SESSION['valid_userID']; 
 
-	photo_upload('profilePhoto', 'users_profile_photo/',$userID, $db, false);
+if(isset($_POST['profilePhoto']))	photo_upload('profilePhoto', 'users_profile_photo/',$userID, $db, false);
 
 
 if(isset($_GET['delete'])){ 
@@ -22,10 +22,55 @@ if(isset($_GET['delete'])){
 }
 
 
-	photo_upload('photo', 'users_photo/',$userID, $db, true);
+if(isset($_POST['photo']))	photo_upload('photo', 'users_photo/',$userID, $db, true);
+
+	if(isset($_POST['submit_edit'])){
+		$name = $_POST['name'];
+		$password = md5($_POST['password']);
+		$email = $_POST['email'];
+		$birthdate = $_POST['birthdate'];
+		$gender = $_POST['gender'];
+		$city = $_POST['city'];
+		$height = $_POST['height'];
+		$education = $_POST['education'];
+
+		if($name!=$_SESSION['valid_user']){
+			$query = "SELECT * FROM users_account WHERE name = '$name';";
+			$result = $db->query($query);
+
+			if ($result->num_rows >0 )
+			  {
+			  	header('Location: profile.php?profile.php?edit=Edit+Profile&username_exist=1');
+			  	exit();
+
+			  }
+		}
 
 
-header('Location: profile.php');
+		$sql = "UPDATE users_account SET name='$name', password='$password', email='$email', 
+				birthdate='$birthdate', gender='$gender',city='$city',height='$height',education='$education'
+				where userID = '$userID'";
+		echo $sql;
+		//	echo "<br>". $sql. "<br>";
+		$result = $db->query($sql);
+
+
+		if (!$result) 
+			echo "Your query failed.";
+		else
+			echo $name . ", Your information was changed";
+
+		
+		$_SESSION['valid_user'] = $name;?>
+		<a href="profile.php">Go back to profile</a>   
+		<?php 
+
+	}else{
+		
+		header('Location: profile.php');
+	}
+
+
 	
 ?>
 
