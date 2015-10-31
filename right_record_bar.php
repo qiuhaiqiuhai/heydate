781a44df0fe16a6d92b2df92b5155521b6f3acbc
qiuhaiqiuhai <qiuhaiqiuhai@126.com>
2015-10-31 12:28:34 +0800

    display in reverse time order

--- right_record_bar.php
+++ right_record_bar.php
@@ -4,7 +4,7 @@
     //query match    
     $query = 'select * from users_account where userID in'.
              '(SELECT userID2 from users_relationship AS X where(userID1 = '.$_SESSION['valid_userID'].' and status="Like")'.
-             ' and userID2 IN (select userID1 from users_relationship where userID2= '.$_SESSION['valid_userID'].' and status="Like") order by statusTime desc)';
+             ' and userID2 IN (select userID1 from users_relationship where userID2= '.$_SESSION['valid_userID'].' and status="Like"))';
 
     //display match
     $result = $db->query($query);
@@ -18,7 +18,7 @@
     }
 
     //recommend
-    $query = 'select * from users_account where city in (select city from users_account where userID='.$_SESSION['valid_userID'].')';
+    $query = 'select * from users_account where userID <> '.$_SESSION['valid_userID'].' and city in (select city from users_account where userID='.$_SESSION['valid_userID'].')';
     $result = $db->query($query);
     if($result->num_rows>0){
                 
@@ -30,7 +30,8 @@
     }
 
     //display who you viewed
-    $query = 'select * from users_account where userID in (select userID2 from users_relationship where userID1='.$_SESSION['valid_userID'].')';
+    $subset = 'select * from users_relationship where userID1='.$_SESSION['valid_userID'];
+    $query = 'select * from users_account right join ('.$subset.') as X on users_account.userID=X.userID2 order by statusTime desc';
     $result = $db->query($query);
     if($result->num_rows>0){
                 
@@ -42,7 +43,8 @@
     }
 
     //display who viewed you
-    $query = 'select * from users_account where userID in (select userID1 from users_relationship where userID2='.$_SESSION['valid_userID'].')';
+    $subset = 'select * from users_relationship where userID2='.$_SESSION['valid_userID'];
+    $query = 'select * from users_account right join ('.$subset.') as X on users_account.userID=X.userID1 order by statusTime desc';
     $result = $db->query($query);
     if($result->num_rows>0){
                 
